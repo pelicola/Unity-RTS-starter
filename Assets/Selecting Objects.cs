e51d5b77beba45e4789b5ca223f7d7fc0d9399cb
peli <100323684+pelicola@users.noreply.github.com>
2023-11-28 22:09:04 -0500

    programming tut edit

---
+++ Assets/Selecting Objects.cs
@@ -0,0 +1,39 @@
+﻿        using UnityEngine;
+
+        public class SelectingObjects : MonoBehaviour
+        {
+            [SerializeField]
+            private LayerMask objectLayer;
+            [SerializeField]
+            private new Camera camera;
+
+            [SerializeField]
+            private Color selectedColor, originalColor;
+            private MeshRenderer lastSelected;
+
+            void Start()
+            {
+                this.camera = Camera.main;
+            }
+
+            void Update()
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out RaycastHit hit, 100, objectLayer))
+                    {
+                        //Can also use Draw Ray
+                        Debug.DrawLine(ray.origin, hit.point, Color.red, 1f);
+
+                        //cache material and colors before changing the color
+                        lastSelected = hit.transform.GetComponent<MeshRenderer>();
+                        originalColor = lastSelected.material.color;
+                        lastSelected.material.color = selectedColor;
+                    }
+                }
+                else if (Input.GetMouseButtonUp(0) && lastSelected != null)
+                    lastSelected.material.color = originalColor;
+            }
+        }
